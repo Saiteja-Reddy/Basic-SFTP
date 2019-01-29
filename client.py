0426e131c20c52f353f96980a0e6c100ca740e34
Saiteja-Reddy <saiteja0317@gmail.com>
2019-01-29 21:28:15 +0530

    Diffie Hellman setup with server and client

--- client.py
+++ client.py
@@ -1,11 +1,37 @@
 import socket
+import MR
+from random import randrange
+from struct import *
+from DH import *
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.sendall(b'Hello, world')
-data = s.recv(1024)
 
-print('Received', repr(data))
+prime, alpha = MR.get_Prime_PR() # public
+print("Generated Prime Number is " + str(prime))
+Xa = randrange(2, prime - 2)
+print("Selected Private Xa as " + str(Xa))
+print("Generator is " + str(alpha))
+Ya = pow(alpha, Xa, prime) # public
+print("Ya is " + str(Ya))
+
+DH_share_pack = create_DH_share_pack(Ya, prime, alpha)
+print("Sending to Server", unpack_DH_share_pack(DH_share_pack))
+
+s.sendall(DH_share_pack)
+DH_reshare_pack = s.recv(calcsize('i'))
+DH_data = unpack_DH_reshare_pack(DH_reshare_pack)
+print(DH_data)
+
+key = pow(DH_data['Yb'], Xa , prime)
+print("key is " + str(key))
+print("Done Diffie-Hellmann!!")
+
+
+
+
+
+# print('Received', repr(data))
--- server.py
+++ server.py
@@ -1,15 +1,33 @@
 import socket
 import threading 
 import time
+import MR
+from random import randrange
+from DH import *
+from struct import *
   
 # thread fuction 
 def threaded(conn): 
-    while True:
-        data = conn.recv(1024)
-        # time.sleep(5)
-        if not data:
-            break
-        conn.sendall(data)
+    DH_share_pack = conn.recv(calcsize('iii'))
+    DH_data = unpack_DH_share_pack(DH_share_pack)
+    print(DH_data)
+    Xb = randrange(2, DH_data['prime'] - 2)
+    print("Selected Private Xb as " + str(Xb))
+    
+    Yb = pow(DH_data['alpha'], Xb, DH_data['prime']) # public to A
+    print("Yb is " + str(Yb)) 
+
+    DH_reshare_pack = create_DH_reshare_pack(Yb)
+    print("Sending to Server", unpack_DH_reshare_pack(DH_reshare_pack))
+    conn.sendall(DH_reshare_pack)
+
+    prime = DH_data['prime']
+    key = pow(DH_data['Ya'], Xb , prime)
+
+    print("key is " + str(key))
+    print("Done Diffie-Hellmann!!\n")
+
+
     conn.close() 
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
